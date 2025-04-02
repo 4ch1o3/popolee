ea9a5ccadbe3e6faa5043a43132f2a11522663c2
ideagalaxy <ideagalaxy@naver.com>
2025-04-02 15:54:41 +0900

    0402

--- popolee/feed/views.py
+++ popolee/feed/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.middleware.csrf import CsrfViewMiddleware
+from datetime import datetime
 
 
+def main_page(request):
+    print(request)
+    #exchange_info = Exchange.objects.all().values()[0]
+    user_pk = request.user.pk
+    user = User.objects.get(id=user_pk)
+    context = {'info' : exchange_info,
+               'user' : user}
+    return render(request, 'main_page.html',context)
