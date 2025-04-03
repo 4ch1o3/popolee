7a06c03cfe18447ec837a033001cb73db3b5a94c
ideagalaxy <ideagalaxy@naver.com>
2025-04-03 11:11:51 +0900

    feet : add sort option in feed app

--- popolee/feed/views.py
+++ popolee/feed/views.py
@@ -5,11 +5,26 @@ from django.contrib import auth
 from django.middleware.csrf import CsrfViewMiddleware
 from datetime import datetime
 
+max_images = 10
 
 def main_page(request): #show images
     print(request)
+    if request.method == "POST":
+        print(request.POST)
+
+        sortoption = request.POST.get("sortoption")
+        print(sortoption)
+        match sortoption:
+            case 'most_like':
+                sort_field = '-likes'
+            case 'show_old':
+                sort_field = 'created_at'
+            case 'show_new':
+                sort_field = '-created_at'
+        posts = Image.objects.order_by(sort_field)[:max_images]
+
     #exchange_info = Exchange.objects.all().values()[0]
-    posts = Image.objects.order_by('created_at')[:30]
+    posts = Image.objects.order_by('likes')[:max_images]
     print(posts)
     context = {'posts' : posts}
     return render(request, 'main_page.html',context)
