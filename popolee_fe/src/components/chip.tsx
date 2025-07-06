909534c9a56d320b707cb645195c37a6cda045c2
4ch1o3 <ljwon413@gmail.com>
2025-07-06 11:51:59 +0900

    wip: init fe env

---
+++ popolee_fe/src/components/chip.tsx
@@ -0,0 +1,5 @@
+export const Chip = ({ label }: { label: string }) => {
+  return (
+    <div className="bg-secondary border-primary text-primary">{label}</div>
+  );
+};
---
+++ popolee_fe/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: "#2F323C",
+      secondary: "#FBFAF9",
+      tertiery: "#7C8195",
+      "text-placeholder": "#999999",
+      "search-bg": "#EFEFEF",
+      error: "#BE3030",
+      like: "#D16D6D",
+      "form-bg": "#rgba(255, 255, 255, 0.8)",
+      "img-placeholder-green": "#C3D8BF",
+      "img-placeholder-pink": "#E0C9C9",
+      "img-placeholder-blue": "#CDD7E2",
+      "img-placeholder-purple": "#DED8E9",
+      "img-placeholder-orange": "#D9CDBE",
+      "img-placeholder-yellow": "#D9D4BE",
+    },
+  },
+};
